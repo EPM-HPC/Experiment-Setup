57d470548c9cfae2f47e830ce72e7d253f7acdd0
elio <2925539854@qq.com>
2022-08-12 18:00:31 +0800

    rewrite rodinia makefile to sm80 and whole run file

---
+++ Rodinia/dep.sh
@@ -0,0 +1,3 @@
+ln -s /usr/local/cuda/lib64/libcudart.so /usr/lib/libcudart.so
+
+sudo apt-get install libgl1-mesa-dev libglu1-mesa-dev libglew-dev libsdl2-dev libsdl2-image-dev libglm-dev libfreetype6-dev libglfw3-dev libglfw3 freeglut3-dev
