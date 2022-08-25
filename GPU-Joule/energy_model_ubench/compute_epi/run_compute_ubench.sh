caafe0ce8f5472ab6df7f34b5e53d8554d2b0c4a
elio <2925539854@qq.com>
2022-08-25 14:44:06 +0800

    scripts modification

---
+++ GPU-Joule/energy_model_ubench/compute_epi/run_compute_ubench.sh
@@ -0,0 +1,18 @@
+#BIN=$1
+
+GPUJoule_dir="/home/wwr/Desktop/ExperimentDataProcess/GPU-Joule"
+
+NumCTA="56"
+NumThd="1024"
+NumIter="1000000"
+
+    for bench in *.out;
+    do
+	echo $bench
+        $GPUJoule_dir/nvml_power_monitor/example/power_monitor 5 > $GPUJoule_dir/energy_model_data/compute_energy/"$bench"_"$NumIter"iter_power.txt &
+        PM_PID=$!
+        ./$bench $NumCTA $NumThd $NumIter 32 > $GPUJoule_dir/energy_model_data/compute_energy/"$bench"_"$NumIter"iter_time.txt
+        sleep 5
+        kill -15 $PM_PID
+        sleep 5
+    done
